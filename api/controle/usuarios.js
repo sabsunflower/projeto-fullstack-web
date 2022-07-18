101e307bf4bbfefa4080472a5d7e91e72eb24fce
sabsunflower <sabrinamatias228@gmail.com>
2022-07-18 09:57:44 -0300

    add git

---
+++ api/controle/usuarios.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var usuario = require('../modelos/usuarios');
+
+function inicio(req, res) {
+    res.status(200).send({
+        message: 'Hello word!'
+    });
+};
+
+function teste(req, res) {
+    res.status(200).send({
+        message: 'Testing node server!'
+    });
+};
+
+module.exports = {
+    inicio,
+    teste
+}
